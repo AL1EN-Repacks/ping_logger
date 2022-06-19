53826015702e1a3f1473315abd46664b1fadd422
AL1EN <107768574+AL1EN-Repacks@users.noreply.github.com>
2022-06-19 14:54:40 +0600

    Version Update1.0-1.0.1

---
+++ Ping Logger v1.0.1 Visual.bat
@@ -0,0 +1,36 @@
+@echo off
+color 0a
+title Ping Logger by AL1EN
+
+echo    ##     ##         ##     #######  ##   ## 
+echo    ##     ##                ##       ###  ## 
+echo   ####    ##       ####     ##       ###  ## 
+echo   ## #    ##         ##     #####    ## # ## 
+echo  ######   ##         ##     ##       ## # ## 
+echo  ##   #   ##         ##     ##       ##  ### 
+echo ###   ##  ######   ######   #######  ##   ##
+echo Name: Ping Logger
+echo Version: 1.0.1 Visual
+echo Author: AL1EN 
+echo Press Ctrl + C to Stop
+echo --------------------------------------------------
+set /p IP= Enter IP/Domain:
+:top
+cls
+echo    ##     ##         ##     #######  ##   ## 
+echo    ##     ##                ##       ###  ## 
+echo   ####    ##       ####     ##       ###  ## 
+echo   ## #    ##         ##     #####    ## # ## 
+echo  ######   ##         ##     ##       ## # ## 
+echo  ##   #   ##         ##     ##       ##  ### 
+echo ###   ##  ######   ######   #######  ##   ##
+echo Name: Ping Logger
+echo Version: 1.0.1 Visual
+echo Author: AL1EN 
+echo Press Ctrl + C to Stop
+echo --------------------------------------------------
+echo
+echo
+echo Loggin Data....................
+ping -t %IP%|cmd /q /v /c "(pause&pause)>nul & for /l %%a in () do (set /p "data=" && echo(!date! !time! !data!)&ping -n 2 localhost>nul" > log.log
+GoTo top
